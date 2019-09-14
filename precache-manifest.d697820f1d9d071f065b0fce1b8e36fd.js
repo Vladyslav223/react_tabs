5c806d63f098b5a9ab7e52719ec412b643593c1c
vladyslav223 <vladyslav223@gmail.com>
2019-09-14 14:17:17 +0300

    Updates

---
+++ precache-manifest.d697820f1d9d071f065b0fce1b8e36fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e1c48f5019c0e7d88054ba02f139598",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "6f7c12b9af71d234a1af",
+    "url": "/react_tabs/static/css/main.c9bc4d45.chunk.css"
+  },
+  {
+    "revision": "9c5ca2f4a5e34b4fd994",
+    "url": "/react_tabs/static/js/2.be491123.chunk.js"
+  },
+  {
+    "revision": "6f7c12b9af71d234a1af",
+    "url": "/react_tabs/static/js/main.bdfb1034.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
